8ab21dd8c484a854fa34d71b5ac4feddf5fa730c
dawit1616 <dawitsidal1616@gmail.com>
2026-06-13 17:40:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let cartCount = 0;
 
+const cartDisplay = document.getElementById("cart");
+
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     cartCount++;
-    alert("Item added to cart 🛒 Total items: " + cartCount);
+
+    if (cartDisplay) {
+      cartDisplay.textContent = "🛒 Cart: " + cartCount;
+    }
   });
 });
